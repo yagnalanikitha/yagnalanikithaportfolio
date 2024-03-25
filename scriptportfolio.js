933e52dc53723393af67a99b98705a7fe398d39d
yagnalanikitha <164871766+yagnalanikitha@users.noreply.github.com>
2024-03-25 16:56:55 +0530

    Add files via upload

---
+++ scriptportfolio.js
@@ -0,0 +1,18 @@
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('sections');
+let navlinks = document.querySelectorAll('header nav a');
+window.onscroll=()=>{
+    sections.forEach(sec=>{
+        let top=window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight - 150;
+        let id = sec.getAttribute('id');
+        
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id +']').classList.add('active')
+            })
+        }
+    })
+}
